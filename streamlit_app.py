f785dca7eafbedb75c42ff6ff8818608f33f7d10
Greg945 <xtimsandx@gmail.com>
2025-03-13 21:31:17 +0100

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ client = genai.Client(api_key="AIzaSyA3iQXk6-M5XQhzLIMO3SfEAKDPRunTHP8")
 google_search_tool = Tool(google_search=GoogleSearch())
 
 lever_mycomp = 0
-search = "false"
+if 'search' not in st.session_state:
+    st.session_state['search'] = False
 
 if 'response' not in st.session_state:
     st.session_state['response'] = ''
@@ -25,28 +26,8 @@ if 'response' not in st.session_state:
 if 'sprache' not in st.session_state:
     st.session_state['sprache'] = 'Deutsch'
 
-if 'queue' not in st.session_state:
-    st.session_state.queue = []
-
-if 'is_waiting' not in st.session_state:
-      st.session_state.is_waiting = False
-
-if 'is_waiting' in globals():
-    st.session_state.is_waiting = is_waiting
-    print("change zu: ", is_waiting)
-
-if 'is_waiting' not in globals():
-        is_waiting = None
-
-
-
-#if st.session_state.is_waiting_def == False:
-#is_waiting = ""
-#    st.session_state.is_waiting_def = True
-
-#if 'is_waiting' not in globals():
-#    print("rewrite")
-#    is_waiting = False
+if 'language_code' not in st.session_state:
+    st.session_state['language_code'] = 'de-DE'
 
 if "context" not in st.session_state:
     st.session_state.context = [{"role": "user", "content": "hallo"}]  #load_chat_history()
@@ -76,8 +57,9 @@ def save_chat_history(chat_history):
         json.dump(chat_history, f)
 
 
-def gemini_request(text, search_enabled=False):
+def gemini_request(text):
     context = st.session_state.context
+    search_enabled=st.session_state.get("search", False)
     try:
         response = client.models.generate_content(
             model="gemini-2.0-flash",
@@ -102,10 +84,6 @@ def gemini_request(text, search_enabled=False):
     print('Apicall for:', text)
     print(response_text)
 
-def delayed_api_call():
-    final_text = " ".join(st.session_state.queue)
-    st.session_state.queue = []
-    gemini_request(final_text, search_enabled=st.session_state.get("search", False))
 
 
 def reset_flag():
@@ -130,31 +108,6 @@ def lang_switch():
     st.session_state.language_code = language_codes.get(st.session_state.sprache, DEFAULT_LANGUAGE)
 
 
-def value_processing(value):
-    event_daten = {"message": "Hallo von Streamlit!"}
-    components.html(f"""
-        <script>
-            window.dispatchEvent(new CustomEvent('streamlit:customEvent', {{detail: {event_daten}}}));
-        </script>
-    """)
-    global is_waiting
-    print("Received", value)
-    print("Waiting: ", st.session_state.is_waiting)
-    print("Waiting2: ", is_waiting)
-    print('halllllllllllllooooooooooo')
-    if value:
-        print("Value da")
-        st.session_state.queue.append(value)
-        if st.session_state.is_waiting == False or is_waiting == False:
-            st.session_state.is_waiting = True
-            is_waiting = True
-            print("          waiting jetzt True")
-            delayed_api_call()
-            thread = Timer(5.0, reset_flag)
-            thread.start()
-        else:
-            print("blocked!!!!!!!!!!!!")
-
 ##################################  UI  #########################################
 
 st.title("STG")
@@ -164,13 +117,9 @@ st.selectbox("Sprache",("Deutsch", "Englisch", "Französisch"), key="sprache", o
 
 
 if st.toggle("STT?"):
-    value = mycomponent(my_input_value="hello there")
+    value = mycomponent(language=st.session_state.language_code)
     st.write("Received", value)
-    value_processing(value)
-
-
-
-
+    gemini_request(value)
 
 
 
@@ -178,20 +127,20 @@ text_input = st.text_input("Oder Eingabe per Tastatur", key="text_input")
 
 if st.button("Senden"):
     if text_input:
-        gemini_request(text_input, search_enabled=st.session_state.get("search", False))
+        gemini_request(text_input)
 
 
 if st.button("Clear Session State"):
     for key in st.session_state.keys():
         del st.session_state[key]
 
-st.session_state.search = st.checkbox("Google-Suche aktivieren", value=st.session_state.get("search", False))
+st.checkbox("Google-Suche aktivieren", key="search")
 
 if st.button("Zufällige Frage testen"):
     test_prompts = ["Apfel", "Was ist die Hauptstadt von Berlin?", "Kirsche", "Hallo", "Wie hoch ist der Eiffelturm"]
     value = random.choice(test_prompts)
     st.write("Received", value)
-    value_processing(value)
+    gemini_request(value)
 
 
 st.link_button("Close", "https://login.schulportal.hessen.de/?url=aHR0cHM6Ly9jb25uZWN0LnNjaHVscG9ydGFsLmhlc3Nlbi5kZS8=&skin=sp&i=5120")
